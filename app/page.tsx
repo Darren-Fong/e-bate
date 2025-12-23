6cea708c3f502bda44d1e523e6fafa614d51bace
Darren Fong <fongdarren1002@icloud.com>
2025-12-23 10:51:26 +0800

    Change logo size.

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
           style={{ display: "block", marginLeft: "auto", marginRight: "auto", marginTop: "10px"}}
           src="/e-bate-logo.png"
           alt="E-Bate Logo"
-          width={500}
-          height={500}
+          width={300}
+          height={100}
         />
         <p style={{ textAlign: "center", fontSize: "30px", fontFamily: "SF Pro Text", fontWeight: "semi-bold" }}>Online platform for debate training.</p>
       </div>
